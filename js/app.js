0891c2e1c48e0051618cfa1de410717070cd0a5b
amnkhan <amnkhan.me@gmail.com>
2021-08-15 00:12:05 +0600

    order confirmation logic done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const code = document.querySelector("#editor");
-  const trigger = document.querySelector(".trigger-btn");
-  code.value = "";
-
+  let form = document.querySelector(".template-form");
+  let textarea = document.querySelector(".editor-textarea");
+  let select = document.querySelector("#select-template");
+  let feedback = document.querySelector(".feedback");
+  textarea.value = "";
+  // show feedback to the user
+  function showFeedback(className, message) {
+    feedback.classList.add(className);
+    feedback.textContent = message;
+    feedback.style.display = "block";
+    setTimeout(function () {
+      feedback.classList.remove(className);
+      feedback.style.display = "none";
+    }, 5000);
+  }
   // Data store
   let state = {
-    // find
-    pplrOrderConfiramtion: `{% if line.variant.title != 'Default Title' %}
-        <span class="order-list__item-variant">{{ line.variant.title }}</span><br/>
+    // order confirmation
+    orderConfirmation: {
+      value: ``,
+      search:
+        /{% if line\.variant\.title != 'Default Title' %}\s*<span class="order-list__item-variant">\s*{{ line\.variant\.title }}\s*<\/span>\s*<br\/>\s*{% endif %}/gim,
+      replace: `
+      {% if line.variant.title != 'Default Title' %}
+        <span class="order-list__item-variant">{{ line.variant.title }}
+        </span>
+        <br/>
       {% endif %}
+
       {% for p in line.properties %}   
       {% assign hidden_property = p.first | first | replace: '_', true %}
       {% unless p.last == blank %} 
       {% if p.first contains 'pdf' %}
-      
       {% assign hidden_property = false%}
       {% assign p.first = p.first | replace: '_' %}
-      
       {% endif %} 
-      
-      
       {% if hidden_property == 'true' %} 
-       <span style="display:none;" class="product-personalizer-line-item-prop" data-prop-name="{{ p.first }}">{{ p.first }}: {{ p.last }}</span> 
+      <span style="display:none;" class="product-personalizer-line-item-prop" data-prop-name="{{ p.first }}">{{ p.first }}: {{ p.last }}
+      </span> 
       {% else %} 
       {{ p.first | replace: '_'}}: 
       {% if p.last contains '/uploads/' or p.last contains '/assets/' or p.last contains '/products/' %} 
@@ -32,21 +48,25 @@ document.addEventListener("DOMContentLoaded", () => {
       {% if p.last contains 'pdf' %}
       {% assign format = 'pdf' %}
       {% endif %}
-      <a target="_blank"  href="{{ p.last }}?format={{ format }}" src="{{ p.last }}?format={{ format }}" class="jslghtbx-thmb" data-jslghtbx download>Download {{ format }} file</a> 
+      <a target="_blank"  href="{{ p.last }}?format={{ format }}" src="{{ p.last }}?format={{ format }}" class="jslghtbx-thmb" data-jslghtbx download>Download {{ format }} file
+      </a> 
       {% else %} 
       {{ p.last | newline_to_br }} 
       {% endif %} 
       <br> 
       {% endif %} 
-      {% endunless %}{% endfor %}`,
-    // replace with
-    orderConfirmationFinder:
-      /{% if line\.variant\.title != 'Default Title' %}\s*<span class="order-list__item-variant">{{ line\.variant\.title }}<\/span><br\/>\s*{% endif %}/gim,
-    // user pasted code
-    userInput: ``,
+      {% endunless %}{% endfor %}
+      `,
+      alreadyExist: /{% for p in line\.properties %}/gim,
+    },
+    // packing slip
+    packingSlip: {
+      value: ``,
+      search: ``,
+      replace: ``,
+    },
   };
-  // https://jsfiddle.net/x7f6bgda/5/
-  //   // Inject the code
+
   const injectCode = () => {
     if (code.value !== "") {
       state.userInput = code.value.toString();
@@ -61,11 +81,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  // Check if the button is clicked
-  trigger.addEventListener("click", injectCode);
-  console.log(typeof state.pplrOrderConfiramtion);
-});
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // check if the textarea value is empty
+    if (textarea.value !== "") {
+      // order confirmation
+      if (select.value == "order-confirmation") {
+        state.orderConfirmation.value = textarea.value.toString();
+        let value = state.orderConfirmation.value;
+        let regex = state.orderConfirmation.search;
+        let replace = state.orderConfirmation.replace;
+        let exist = state.orderConfirmation.alreadyExist;
 
-// let string = "ABC";
-// let newString = string.repeat(A , D);
-// DBC
+        // check if its a order template
+        if (value.match(regex) != null) {
+          let replaced = value.replace(regex, replace);
+          textarea.value = replaced;
+          showFeedback(
+            "alert-success",
+            "😀 Sucessfull added product personalizer code"
+          );
+        } else {
+          showFeedback("alert-danger", "😞 Not a Order Confirmation template");
+        }
+      }
+      // packing slip
+      if (select.value == "packing-slip") {
+        console.log("You have selected order packing-slip template");
+      }
+      // shipping confirmation
+      if (select.value == "shipping-confirmation") {
+        console.log("You have selected order shipping-confirmation template");
+      }
+    } else {
+      showFeedback("alert-danger", "Please paste your template.");
+    }
+  });
+});
